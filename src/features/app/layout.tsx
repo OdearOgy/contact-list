79c47e5a17004775464cffc007466933c1fa8747
OdearOgy <hovhan198@gmail.com>
2025-01-20 00:11:10 +0400

    change layout

--- src/features/app/layout.tsx
+++ src/features/app/layout.tsx
@@ -1,9 +1,8 @@
 import { FunctionComponent, PropsWithChildren } from "react";
-import { Cover } from "../../components";
 
 // TODO (hom): Add header
 const Layout: FunctionComponent<PropsWithChildren> = ({ children }) => {
-  return <Cover>{children}</Cover>;
+  return children;
 };
 
 export default Layout;

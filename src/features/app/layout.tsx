94cfaf6469a0941381ae7e5a210858389b944a12
Hovhannes <hovhan198@gmail.com>
2025-01-19 14:26:20 +0000

    Setup tanstack

---
+++ src/features/app/layout.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent, PropsWithChildren } from "react";
+
+const Layout: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  return children;
+};
+
+export default Layout;
